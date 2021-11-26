4bd89e8eb8e2457ceba46f28bd435bb3ba5b3b14
Gustavo Galli <gustavosgalli@gmail.com>
2021-11-26 11:24:56 -0300

    Subindo atividade 4-1

---
+++ atividade04/04_atividade1.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE db_generation_game_online;
+USE db_generation_game_online;
+
+CREATE TABLE tb_classe(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(15),
+    magia BOOLEAN
+);
+
+INSERT INTO tb_classe (nome, magia) VALUES
+("Arqueiro", FALSE),
+("Mago", TRUE),
+("Guerreiro", FALSE),
+("Anão", fALSE),
+("Goblin", tRUE);
+
+CREATE TABLE tb_personagens(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(10),
+    ataque INT,
+    defesa INT,
+    id_classe INT,
+    FOREIGN KEY (Id_classe) REFERENCES tb_classe (Id)
+);
+
+INSERT INTO tb_personagens (nome, ataque, defesa, id_classe) VALUES
+("Legolas", 1000, 800, 1),
+("Gandalf", 3000, 2500, 2),
+("Aragorn", 2000, 1800, 3),
+("Gimli", 1000, 1100, 4),
+("Azog", 900, 400, 5),
+("Boromir", 1500, 1200, 3),
+("Saruman", 2900, 3000, 2),
+("Sauron", 10000, 10000, 3);
+
+SELECT * FROM tb_personagens WHERE ataque > 2000;
+SELECT * FROM tb_personagens WHERE defesa > 1000 AND defesa < 2000;
+SELECT * FROM tb_personagens WHERE nome LIKE "G%";
+SELECT * from tb_personagens INNER JOIN tb_classe ON tb_personagens.id_classe = tb_classe.id;
+SELECT * from tb_personagens INNER JOIN tb_classe ON tb_personagens.id_classe = tb_classe.id WHERE tb_classe.nome = "Guerreiro";
